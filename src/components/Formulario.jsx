43edee4a4ec6dc15e8291ba2ba0044c405e9b77f
Matías <matiasmartinezvelasquez@gmail.com>
2023-09-09 19:20:45 -0300

    Se agrega alerta de exito

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -11,6 +11,7 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
   });
 
   const [mostrarAlerta, setMostrarAlerta] = useState(false);
+  const [mostrarAlertaExito, setMostrarAlertaExito] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,6 +28,7 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
       !validarEdad(colaborador.edad)
     ) {
       setMostrarAlerta(true);
+      setMostrarAlertaExito(false);
     } else {
       agregarColaborador(colaborador);
       setColaborador({
@@ -37,19 +39,21 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
         telefono: "",
       });
       setMostrarAlerta(false);
+      setMostrarAlertaExito(true);
     }
   };
-// validando la semantica del werito
+
+  // validando la semantica del werito
   const validarCorreo = (correo) => {
     const regexCorreo = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     return regexCorreo.test(correo);
   };
-// validando el cel del werito
+  // validando el cel del werito
   const validarTelefono = (telefono) => {
     const regexTelefono = /^\d{9}$/;
     return regexTelefono.test(telefono);
   };
-// validando edad del werito
+  // validando edad del werito
   const validarEdad = (edad) => {
     const edadNumero = parseInt(edad, 10);
     return !isNaN(edadNumero) && edadNumero >= 18 && edadNumero <= 130;
@@ -70,7 +74,7 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
           />
         </div>
         <div className="form-group">
-        <label>Correo:</label>
+          <label>Correo:</label>
           <input
             type="email"
             className={`form-control ${
@@ -87,7 +91,7 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
           )}
         </div>
         <div className="form-group">
-        <label>Edad:</label>
+          <label>Edad:</label>
           <input
             type="number"
             className={`form-control ${
@@ -100,7 +104,10 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
             onChange={handleChange}
           />
           {mostrarAlerta && !validarEdad(colaborador.edad) && (
-            <Alert mensaje="Por favor ingrese una edad válida (entre 18 y 130 años)" tipo="danger" />
+            <Alert
+              mensaje="Por favor ingrese una edad válida (entre 18 y 130 años)"
+              tipo="danger"
+            />
           )}
         </div>
         <div className="form-group">
@@ -114,7 +121,7 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
           />
         </div>
         <div className="form-group">
-        <label>Teléfono:</label>
+          <label>Teléfono:</label>
           <input
             type="tel"
             className={`form-control ${
@@ -127,7 +134,10 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
             onChange={handleChange}
           />
           {mostrarAlerta && !validarTelefono(colaborador.telefono) && (
-            <Alert mensaje="Por favor ingrese un teléfono válido de 9 dígitos" tipo="danger" />
+            <Alert
+              mensaje="Por favor ingrese un teléfono válido de 9 dígitos"
+              tipo="danger"
+            />
           )}
         </div>
         <button type="submit" className="btn btn-primary">
@@ -135,7 +145,16 @@ const Formulario = ({ agregarColaborador, mensajeAlert, tipoAlert }) => {
         </button>
       </form>
       {mostrarAlerta && (
-        <Alert mensaje="Por favor complete todos los campos correctamente" tipo="danger" />
+        <Alert
+          mensaje="Por favor complete todos los campos correctamente"
+          tipo="danger"
+        />
+      )}
+      {mostrarAlertaExito && (
+        <Alert
+          mensaje="El colaborador se ha agregado con éxito"
+          tipo="success"
+        />
       )}
     </div>
   );
